08d7dd49f3cb389ba370be124664c0aa60403448
parkyounbae <yache1217@naver.com>
2023-11-12 15:29:32 +0900

    loginFeature branch added

---
+++ app/src/main/java/com/example/dailyfootprint/ui/login/StartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dailyfootprint.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dailyfootprint.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
