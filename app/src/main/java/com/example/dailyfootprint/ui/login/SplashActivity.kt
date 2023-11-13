7885a0d41736a0c5be060f7c917724e521a2544c
parkyounbae <yache1217@naver.com>
2023-11-14 00:33:02 +0900

    [bug] 구글 로그인 정보에는 등록되어 있지만 데이터베이스에 없는 경우 회원가입 화면으로 넘어가도록 수정하였습니다.

--- app/src/main/java/com/example/dailyfootprint/ui/login/SplashActivity.kt
+++ app/src/main/java/com/example/dailyfootprint/ui/login/SplashActivity.kt
@@ -1,12 +1,17 @@
 package com.example.dailyfootprint.ui.login
 
+import FirebaseManager.userDatabaseReference
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import com.example.dailyfootprint.MainActivity
 import com.example.dailyfootprint.R
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 
 class SplashActivity : AppCompatActivity() {
     private val SPLASH_DELAY: Long = 3000 // 3초
@@ -19,18 +24,53 @@ class SplashActivity : AppCompatActivity() {
         }, SPLASH_DELAY)
     }
 
-    private fun checkFirebaseAccount() {
-        val firebaseManager = FirebaseManager.getFirebaseAuthInstance()
+    fun gotoMainScreen() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
 
-        // 파이어베이스 계정 정보 확인 로직 추가
-        if (firebaseManager.getCurrentUser() != null) {
-            // 계정이 있으면 홈 화면으로 이동
-            startActivity(Intent(this, MainActivity::class.java))
+    fun gotoStartScreen() {
+        startActivity(Intent(this, StartActivity::class.java))
+    }
+
+    fun gotoSignUpScreen() {
+        startActivity(Intent(this, SignUpActivity::class.java))
+    }
+
+    private fun checkFirebaseAccount() {
+
+        val currentUser = FirebaseManager.getFirebaseAuthInstance().currentUser
+
+        if (currentUser != null) {
+            // 이미 로그인한 사용자인 경우
+            val uid = currentUser.uid
+
+            // Realtime Database에서 사용자 정보 확인
+            userDatabaseReference.child(uid).addListenerForSingleValueEvent(object :
+                ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        Log.w("already exist","in user")
+                        // 이미 등록된 사용자인 경우
+                        gotoMainScreen()
+                    } else {
+                        Log.w("already exist","not in user")
+                        // 등록되지 않은 사용자인 경우
+                        gotoSignUpScreen()
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // 데이터베이스 조회 실패 시 처리
+                    Log.w("onCancelled","error")
+                }
+            })
         } else {
-            // 계정이 없으면 초기 화면으로 이동
-            startActivity(Intent(this, StartActivity::class.java))
+            // 로그인하지 않은 경우
+            // 여기서 로그인 처리를 진행하고, 로그인 성공 후 위의 로직을 반복합니다.
+            Log.w("not already exist","in user")
+            gotoStartScreen()
+
         }
 
-        finish() // 현재 액티비티 종료
     }
 }
