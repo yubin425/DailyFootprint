3c9e9e0aeaf689ab386914a65823b740e0ab490a
Kim Sunoh <sunoh1222@gmail.com>
2023-11-16 20:18:00 +0900

    [feat] 친구탭 + 버튼 UI 추가
    친구 탭 내 + 버튼을 검정 색 원, 흰 색 '+' 기호 버튼으로, 우측 하단에 추가했습니다.
    추가로 기존 notifications로 표기된 class 이름을 모두 friends로 변경하였습니다.

--- app/src/main/java/com/example/dailyfootprint/ui/friends/FriendsViewModel.kt
+++ app/src/main/java/com/example/dailyfootprint/ui/friends/FriendsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.dailyfootprint.ui.notifications
+package com.example.dailyfootprint.ui.friends
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class FriendsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is Friends Fragment"
     }
     val text: LiveData<String> = _text
 }
