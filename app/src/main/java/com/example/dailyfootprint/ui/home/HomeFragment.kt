562e68628a9c5aa5b1f9870704e6f34576c3baeb
spspd <zip02934@hanyang.ac.kr>
2023-11-17 13:07:20 +0900

    [feat] UI 수정, main의 세부화면 연결
    디자인 미완료

--- app/src/main/java/com/example/dailyfootprint/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/dailyfootprint/ui/home/HomeFragment.kt
@@ -8,10 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.dailyfootprint.databinding.FragmentHomeBinding
+import com.example.dailyfootprint.databinding.HomeMainBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+//    private var _binding: FragmentHomeBinding? = null
+    private var _binding: HomeMainBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +27,13 @@ class HomeFragment : Fragment() {
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = HomeMainBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+//        val textView: TextView = binding.textHome
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
         return root
     }
 
