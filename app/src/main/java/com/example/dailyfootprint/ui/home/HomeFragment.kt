a8fecaa1dec10893ef95a8d5490a71075a333d37
spspd <zip02934@hanyang.ac.kr>
2023-11-19 07:22:35 +0900

    [feat] gps 인증기능 추가
    GPS 인증, 리사이클러뷰 반영
    이후 수정해야할일 HOMEFRAGMENT에 적어두었음

--- app/src/main/java/com/example/dailyfootprint/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/dailyfootprint/ui/home/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.example.dailyfootprint.ui.home
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.drawable.Drawable
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -26,45 +30,76 @@ import com.example.dailyfootprint.R
 import com.example.dailyfootprint.databinding.FragmentHomeBinding
 import com.example.dailyfootprint.databinding.HomeMainBinding
 import com.example.dailyfootprint.databinding.HomeRecyclerViewItemBinding
+import com.example.dailyfootprint.model.Challenge
+import com.example.dailyfootprint.model.User
 
 
 val dataList = listOf("항목 1", "항목 2", "항목 3","항목 4","항목 5","항목 6","항목 7","항목 8","항목 9")
+val exampleChallenge = Challenge(
+    challengeCode = "CH001",
+    challengeName = "Marathon Training", // home에서 사용
+    challengeOwner = "Alice",
+    position = arrayOf(37.7749F, -122.4194F), // home에서 사용
+    goal = 42, // 마라톤의 길이 (킬로미터)
+    successTime = arrayOf(0, 0, 0, 1, 1, 1, 1) // 주중에만 도전 (예: 수요일부터 일요일까지)
+)
+val datachalllist = listOf(exampleChallenge, exampleChallenge, exampleChallenge, exampleChallenge, exampleChallenge, exampleChallenge, exampleChallenge)
+// challenge형식으로 받아와서 넣기
+// 위치사용
+// 성공과 반응
+// 데이터 올리는 것 까지 <- 문서와 맞춰야함
 
-class MyAdapter(private val dataList: List<String>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+// 임시데이터
 
+class MyAdapter(private val dataList: List<Challenge>) : RecyclerView.Adapter<MyAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = HomeRecyclerViewItemBinding.inflate(inflater, parent, false)
 
-        val myButton: Button = binding.certifiyButton
-        var Button_value = true
-        myButton.setOnClickListener {
-            // 여기에 버튼 클릭시 수행할 작업을 넣습니다.
-            Toast.makeText( myButton.context, binding.textTitle.text.toString()+" "+myButton.text.toString() + " 버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show()
-            Button_value = false
-            if(!Button_value){
-
-                myButton.isEnabled = Button_value
-
-                myButton.background= ContextCompat.getDrawable(myButton.context, R.drawable.round_gray_button)
-            }
-
-        }
 
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = dataList[position]
+        val item = datachalllist[position]
         holder.bind(item)
     }
 
-    override fun getItemCount() = dataList.size
+    override fun getItemCount() = datachalllist.size
 
     class ViewHolder(private val binding: HomeRecyclerViewItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: String) {
-            binding.textTitle.text = item
+        fun bind(item: Challenge) {
+            binding.textTitle.text = item.challengeName
+
+            val myButton: Button = binding.certifiyButton
+            var button_value = true
+            myButton.setOnClickListener {
+                // 여기에 버튼 클릭시 수행할 작업을 넣습니다.
+
+                button_value = false
+                if(!button_value){
+
+                    val locationChecker = LocationChecker(myButton.context)
+
+                    val (latitude, longitude) = item.position
+//                    val (latitude, longitude) = Pair(37.4219983, -122.084) // 현재위치 뉴욕
+                    val latitudeDouble = latitude.toDouble()
+                    val longitudeDouble = longitude.toDouble()
+                    val isNearby = locationChecker.isWithin20mOfTarget(latitudeDouble, longitudeDouble)
+
+                    if (isNearby) {
+                        myButton.isEnabled = button_value
+                        // 20미터 이내인 경우의 로직
+                        Toast.makeText( myButton.context, binding.textTitle.text.toString()+" "+myButton.text.toString() + " 버튼이 클릭:true", Toast.LENGTH_SHORT).show()
+                        myButton.background= ContextCompat.getDrawable(myButton.context, R.drawable.round_gray_button)
+                    } else {
+                        // 20미터 밖인 경우의 로직
+                        Toast.makeText( myButton.context,latitude.toString() + ", " + longitude, Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+            }
             // 다른 뷰에 데이터 바인딩
         }
     }
@@ -111,7 +146,8 @@ class HomeFragment : Fragment() {
 //        recyclerView.addItemDecoration(dividerItemDecoration)
         recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
-            adapter = MyAdapter(dataList)
+//            adapter = MyAdapter(dataList)
+            adapter = MyAdapter(datachalllist)
 //            isNestedScrollingEnabled = false
         }
 
@@ -164,3 +200,25 @@ class CustomDividerDecoration(context: Context) : RecyclerView.ItemDecoration() 
         }
     }
 }
+
+class LocationChecker(private val context: Context) {
+
+    private val locationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+    fun isWithin20mOfTarget(targetLatitude: Double, targetLongitude: Double): Boolean {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 권한이 없다면 false 반환
+            return false
+        }
+
+        val currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER) ?: return false
+        val targetLocation = Location("").apply {
+            latitude = targetLatitude
+            longitude = targetLongitude
+        }
+
+        val distanceInMeters = currentLocation.distanceTo(targetLocation)
+        return distanceInMeters <= 20
+    }
+}
+
