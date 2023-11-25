19474508efb34986bdce8bb31b2ad6f273647381
JYunL <nov27yun@naver.com>
2023-11-25 22:37:47 +0900

    [feat] firebase 연동 완, 프로그래스바(초록) 완

--- app/src/main/java/com/example/dailyfootprint/model/Challenge.kt
+++ app/src/main/java/com/example/dailyfootprint/model/Challenge.kt
@@ -1,10 +1,19 @@
 package com.example.dailyfootprint.model
 
+import androidx.annotation.Keep
+
+@Keep
 data class Challenge(
     val challengeCode : String,
     val challengeName : String,
     val challengeOwner : String,
-    val position : Array<Float> = Array(2) { 0.0F },
+    //val position : Array<Float> = Array(2) { 0.0F },
+    val position: ArrayList<Float> = arrayListOf(0.0F, 0.0F),
     val goal : Int,
-    val successTime : Array<Int> = Array(7) {0}
-)
+    //val successTime : Array<Int> = Array(7) {0}
+    val successTime: ArrayList<Int> = arrayListOf(0, 0, 0, 0, 0, 0, 0)
+
+) {
+    constructor() : this("", "", "", arrayListOf(0.0F, 0.0F), 0, arrayListOf(0, 0, 0, 0, 0, 0, 0))
+}
+
--- app/src/main/java/com/example/dailyfootprint/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/dailyfootprint/ui/dashboard/DashboardFragment.kt
@@ -1,35 +1,51 @@
 package com.example.dailyfootprint.ui.dashboard
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import com.example.dailyfootprint.ui.dashboard.ChallengeActivity
 import com.example.dailyfootprint.databinding.FragmentDashboardBinding
 import com.example.dailyfootprint.databinding.ActivityChallengeBinding
+
 
 import com.example.dailyfootprint.R
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager.VERTICAL
+import com.example.dailyfootprint.model.Challenge
+import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
 
 class MyAdapter() :
     RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+    private val challengeList = mutableListOf<Challenge>()
+
+
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         public var challengeTitle: TextView = itemView.findViewById(R.id.textView)
         public var location: TextView = itemView.findViewById(R.id.textView2)
         public var numOfChallenge: TextView = itemView.findViewById(R.id.textView3)
         val delChallBtn: Button = itemView.findViewById(R.id.delBtn)
-
     }
 
     // 1. Create new views (invoked by the layout manager)
@@ -48,11 +64,57 @@ class MyAdapter() :
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.challengeTitle.setText("제목입니다")
+
+        //holder.challengeTitle.setText("제목입니다")
         holder.location.setText("위치입니다")
         holder.numOfChallenge.setText("달성 목표 수입니다")
 
-        // Set a click listener for the delete button
+        val challenge = challengeList[position]
+        holder.challengeTitle.text = challenge.challengeName
+        //holder.location.text = challenge.location
+        holder.numOfChallenge.text = "목표: ${challenge.goal}"
+
+        //val drawable = (GradientDrawable) ContextCompat.getDrawable(this, R.drawable.dashboard_challenge_progressbarstart)
+        val progressView = holder.itemView as ViewGroup
+        val dayColors = mapOf(
+            R.id.bar_sun to challengeList[position].successTime[0],
+            R.id.bar_mon to challengeList[position].successTime[1],
+            R.id.bar_tue to challengeList[position].successTime[2],
+            R.id.bar_wed to challengeList[position].successTime[3],
+            R.id.bar_thu to challengeList[position].successTime[4],
+            R.id.bar_fri to challengeList[position].successTime[5],
+            R.id.bar_sat to challengeList[position].successTime[6]
+        )
+
+        Log.d("MyAdapter", "position: $position")
+        Log.d("MyAdapter", "dayColors: $dayColors")
+
+        for ((dayViewId, success) in dayColors) {
+            val dayView: TextView = progressView.findViewById(dayViewId)
+            if (success == 1) {
+                if (dayViewId == R.id.bar_sun) {
+                    val drawable = ContextCompat.getDrawable(
+                        progressView.context,
+                        R.drawable.dashboard_challenge_successstart
+                    )
+                    dayView?.background = drawable
+                }
+
+                else if (dayViewId == R.id.bar_sat) {
+                    val drawable = ContextCompat.getDrawable(
+                        progressView.context,
+                        R.drawable.dashboard_challenge_successend
+                    )
+                    dayView?.background = drawable
+                }
+
+                else {
+                    dayView?.setBackgroundColor(Color.parseColor("#52B449"))
+                }
+            }
+        }
+
+        // 팝업 창
         holder.delChallBtn.setOnClickListener {
             val context = holder.itemView.context
             val intent = Intent(context, ChallengeActivity::class.java)
@@ -61,12 +123,47 @@ class MyAdapter() :
     }
 
 
+    private fun fetchChallenges() {
+        val firebaseDatabaseUrl = "https://dailyfootprint-aeac7-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        val database = Firebase.database(firebaseDatabaseUrl)
+        val challRef = database.reference.child("challenges")
+
+        //currentUser 랑 challengeOwner 확인하는거 추가해야함
+
+        challRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                challengeList.clear()
+
+                for (challengeSnapshot in dataSnapshot.children) {
+                    val challenge = challengeSnapshot.getValue(Challenge::class.java)
+                    challenge?.let {
+                        challengeList.add(it)
+                    }
+                }
+                //updateDayColors()
+                notifyDataSetChanged()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("Firebase", "데이터 가져오기 실패: ${databaseError.toException()}")
+            }
+        })
+
+        // updateDayColors 함수 추가
+
+    }
+
+    fun initialize() {
+        fetchChallenges()
+    }
+
     // 3. Return the size of your dataset (invoked by the layout manager)
     //테스트 (수정필요)
     override fun getItemCount(): Int {
-        return 3
+        return challengeList.size
     }
 }
+
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
@@ -77,7 +174,6 @@ class DashboardFragment : Fragment() {
 
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: LinearLayoutManager
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -92,12 +188,15 @@ class DashboardFragment : Fragment() {
         viewManager = LinearLayoutManager(context, VERTICAL, false)
 
         viewAdapter = MyAdapter()
+        Log.d("DashboardFragment", "MyAdapter instance created: $viewAdapter")
+        (viewAdapter as MyAdapter).initialize()
 
         val recyclerView: RecyclerView = binding.recyclerviewMain
         recyclerView.apply {
           layoutManager = viewManager
             adapter = viewAdapter
         }
+
 
         /*   챌린지 추가 버튼 클릭 시, 챌린지 추가 화면으로 이동
         val challAddBtn: Button = view.findViewById(R.id.addchallnege_btn)
@@ -113,5 +212,6 @@ class DashboardFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        // 데이터베이스 리스너 해제
     }
 }
